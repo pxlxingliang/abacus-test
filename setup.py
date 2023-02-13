12ed72d2c5fa9fb302ce7b76d13ee60c7679b9bf
root <pxlppq007@163.com>
2023-02-13 10:56:41 +0800

    update setup

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setuptools.setup(
     packages=["abacustest"],
     package_dir={"abacustest":"src"},
     package_data = {"abacustest":["abacustest.py","main.py","collectdata.py","lib_collectdata/*","myflow/*","lib_collectdata/*/*","outresult.py"]},
-    entry_points = {'console_scripts': ['abacustest = abacustest.main:main']}
+    entry_points = {'console_scripts': ['abacustest = abacustest.main:main'],},
+    python_requires='>=3.6',
+    install_requires=["pydflow>=1.6","numpy"]
 )
