7d7458ab8e5af7514256395b99cec0b5d500270e
root <pxlxingliang>
2025-09-11 11:50:05 +0800

    fix bug in __init__.py

--- abacustest/__init__.py
+++ abacustest/__init__.py
@@ -1,2 +1,7 @@
-from abacustest.lib_prepare.abacus import ReadInput, WriteInput, AbacusStru, ReadKpt, WriteKpt
-from abacustest.lib_collectdata.collectdata import RESULT
+import traceback
+try:
+    from abacustest.lib_prepare.abacus import ReadInput, WriteInput, AbacusStru, ReadKpt, WriteKpt
+    from abacustest.lib_collectdata.collectdata import RESULT
+except ImportError:
+    traceback.print_exc()
+    print("Import Error in abacustest/__init__.py")
