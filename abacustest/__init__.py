ed64683b188138d8c08ecfae9d7706ceac85862b
root <pxlxingliang>
2026-01-08 11:29:10 +0800

    support to import AbacusSTRU directly from abacustest

--- abacustest/__init__.py
+++ abacustest/__init__.py
@@ -2,6 +2,7 @@ import traceback
 try:
     from abacustest.lib_prepare.abacus import ReadInput, WriteInput, AbacusStru, ReadKpt, WriteKpt
     from abacustest.lib_collectdata.collectdata import RESULT
+    from abacustest.lib_prepare.stru import AbacusSTRU
 except ImportError:
     traceback.print_exc()
     print("Import Error in abacustest/__init__.py")
